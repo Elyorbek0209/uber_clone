e0c169041f166a211b65b55a33e2811f502e0fbe
Elyorbek Soliev <elyorbeksoliev@gmail.com>
2019-07-17 19:41:28 -0400

    yes

--- lib/requests/google_maps_request.dart
+++ lib/requests/google_maps_request.dart
@@ -2,9 +2,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-const apiKey ="AIzaSyDYKfN2r-QUQ9GKijO8DPWXNtEpZaRCrAQ";
-
-
+const apiKey ="APY_KEY";
 
 
 
@@ -12,7 +10,7 @@ class GoogleMapsServices{
 
   Future<String> getRouteCoordinates(LatLng l1, LatLng l2)async{
 
-    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=apiKey"
+    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=apiKey";
 
     http.Response response = await http.get(url);
 
